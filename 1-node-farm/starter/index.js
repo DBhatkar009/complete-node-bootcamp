044c4b46a8f44fdf26541797337373c2d1a08002
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2025-06-25 15:48:18 +0530

    Read write asynchronous function

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,30 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync("./txt/input.txt", 'utf-8');
-console.log(textIn);
+//Blocking, synchronous way 
+// const textIn = fs.readFileSync("./txt/input.txt", 'utf-8');
+// console.log(textIn);
 
-const textOut = `This is what about Avacado🥑 for: ${textIn}. \n Created On ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut); 
+// const textOut = `This is what about Avacado🥑 for: ${textIn}. \n Created On ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut); 
 
 // const fname = "Dhananjay Bhatkar"
 // console.log(fname);
+
+// non-Blocking asynchronous way
+fs.readFile("./txt/start.txt", 'utf-8', (err, data1) => {
+    if(err) return console.log("ERROR..😮");
+     
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+            fs.readFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err =>{
+                console.log('your data just written 😊');
+                
+            });
+        });
+    });
+});
+
+console.log('this will read');
+
